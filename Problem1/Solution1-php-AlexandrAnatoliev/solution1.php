0fe3fe9e717ed8f72527d7236d92d701a1855986
AlexandrAnatoliev <per-1986@list.ru>
2026-03-19 22:10:23 +0300

    docs(Problem1): add comments

--- Problem1/Solution1-php-AlexandrAnatoliev/solution1.php
+++ Problem1/Solution1-php-AlexandrAnatoliev/solution1.php
@@ -1,9 +1,15 @@
 <?php
-  $answer = 0;
-  for ($num = 1; $num < 1000; $num++) {
-    if ($num % 3 == 0 || $num % 5 == 0) {
-      $answer += $num;
-    } 
-  }
-  echo 'Answer = ' . $answer;
+/* 
+ * Первая задача проекта Эйлер
+ * @author AlexandrAnatoliev
+ * @version 0.1.4 2026-03-19
+ */
+
+$answer = 0;
+for ($num = 1; $num < 1000; $num++) {
+  if ($num % 3 == 0 || $num % 5 == 0) {
+    $answer += $num;
+  } 
+}
+echo 'Answer = ' . $answer;
 ?>
