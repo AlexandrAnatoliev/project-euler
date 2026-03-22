cfaee4d6faec6041126fe4690673bc232876a30c
AlexandrAnatoliev <per-1986@list.ru>
2026-03-22 15:08:08 +0300

    refactor: rename project directory names
    version 0.1.4

--- Problem1/AlexandrAnatoliev-php/solution1.php
+++ Problem1/AlexandrAnatoliev-php/solution1.php
@@ -6,10 +6,10 @@
  */
 
 $answer = 0;
-for ($num = 1; $num < 1000; $num++) {
-  if ($num % 3 == 0 || $num % 5 == 0) {
-    $answer += $num;
-  } 
+for ($num = 1; $num < 1000; $num++) {   
+    if ($num % 3 == 0 || $num % 5 == 0) {
+        $answer += $num;
+    } 
 }
 echo 'Answer = ' . $answer;
 ?>
