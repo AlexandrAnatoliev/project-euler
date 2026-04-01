df17e232accef5c7a5675a3b449a05a22f043f58
AlexandrAnatoliev <per-1986@list.ru>
2026-04-01 19:59:16 +0300

    fix(Problem1): fix sonarqube issues

--- Problem1/AlexandrAnatoliev-php/solution1.php
+++ Problem1/AlexandrAnatoliev-php/solution1.php
@@ -1,15 +1,14 @@
 <?php
-/* 
+/*
  * Первая задача проекта Эйлер
  * @author AlexandrAnatoliev
- * @version 0.1.4 2026-03-19
+ * @version 0.1.9 2026-04-01
  */
 
 $answer = 0;
-for ($num = 1; $num < 1000; $num++) {   
+for ($num = 1; $num < 1000; $num++) {
     if ($num % 3 == 0 || $num % 5 == 0) {
         $answer += $num;
-    } 
+    }
 }
 echo 'Answer = ' . $answer;
-?>
