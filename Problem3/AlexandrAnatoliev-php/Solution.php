6ecaf147115719452b786d085f48262dd4c09e19
Alexandr Anatoliev <per-1986@list.ru>
2026-06-22 16:42:59 +0000

    fix(Problem3): add getMinPrimeDiv method

--- Problem3/AlexandrAnatoliev-php/Solution.php
+++ Problem3/AlexandrAnatoliev-php/Solution.php
@@ -5,7 +5,23 @@ namespace Problem3AlexandrAnatoliev;
 class Solution {
   public function getMinPrimeDiv(
     int $num, 
-    int $startNum
+    int $startDiv
   ): int {
+    if(($num % $startDiv == 0) 
+      && ($startDiv * $startDiv <= $num)) {
+      return $startDiv;
+    }
+
+    $div = $startDiv % 2 == 0
+      ? $startDiv + 1
+      : $startDiv;
+
+    for($div; $div * $div <= $num + 1; $div += 2) {
+      if($num % $div == 0) {
+        return $div;
+      }
+    }
+
+    return 1;
   }
 }
